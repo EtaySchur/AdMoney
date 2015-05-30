598068701a3c813b1a45ca0bf7870487ef3a6ecc
EtayKampyle <etay.schur@kampyle.com>
2015-05-30 15:33:28 +0300

    add

--- management.php
+++ management.php
@@ -159,5 +159,6 @@
 				<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
 			</div>
 		</div>
+
 	</body>
 </html>
