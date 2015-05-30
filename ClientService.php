73008c2cb494e8c18f1947edd2b1fc92b96c51ee
EtayKampyle <etay.schur@kampyle.com>
2015-05-30 15:48:58 +0300

    add

--- ClientService.php
+++ ClientService.php
@@ -6,6 +6,7 @@ require_once 'server/db/manager.php';
 if (($_POST) || (isset($_POST))) {
 	$user_id = json_decode(file_get_contents("php://input"));
 	$result = DbManager::getUserDetails($user_id);
+    var_dump($result);
 			echo json_encode($result);
 			exit;
 	
