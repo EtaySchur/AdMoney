db0c869105244d9e28a176222e6fb1159f35028e
EtayKampyle <etay.schur@kampyle.com>
2015-06-05 23:55:12 +0300

    hard coded user id

--- server/db/manager.php
+++ server/db/manager.php
@@ -382,8 +382,7 @@ class DbManager {
         $sql->bindParam('userId', $user_id);
         $sql->execute();
         $user = $sql->fetch(PDO::FETCH_ASSOC);
-        var_dump("user");
-        var_dump($user); exit;
+
 
         /* Getting User Categories */
         $sql = $conn->prepare("SELECT * from app_users2categories
